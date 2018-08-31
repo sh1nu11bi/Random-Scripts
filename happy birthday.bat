87402b41f80defb45eaabc55d4c738a7699e2816
Kale <13501991+AlbusNoir@users.noreply.github.com>
2018-08-30 23:48:38 -0400

    Add files via upload
    Batch file to Print Happy Birthday with a cake! Seriously do not know why I made it but hell, it's enjoyable to watch (also color is easily changeable by just changing the colourcode from a to whatever)

---
+++ happy birthday.bat
@@ -0,0 +1,175 @@
+@echo off
+color a
+
+ping localhost -n 2 >nul
+
+echo .MMMM-      `MMMM:       
+echo .MMMM-      `MMMM:       
+echo .MMMM-      `MMMM:       
+echo .MMMM/-------MMMM:       
+echo .MMMMMMMMMMMMMMMM:       
+echo .MMMMhyyyyyyhMMMM:       
+echo .MMMM-      `MMMM:       
+echo .MMMM-      `MMMM:       
+echo .MMMM-      `MMMM:       
+echo `oooo.       oooo.   
+
+ ping localhost -n 2 >nul
+ cls
+ 
+echo    mms           mms        `hmd-                                      
+echo    MMy           MMy       `oMhMd`                                     
+echo    MMy           MMy       +Nh.oMy`                                    
+echo    MMy`          MMy      :NN. `hMs                                    
+echo    MMNmmmmmmmmmmmMMy     -mN/   .mM+                                   
+echo    MMmoooooooooooMMy    `mMy-....+NN/                                  
+echo    MMy           MMy   `yMMMMMMMMMMMN.                                 
+echo    MMy           MMy   sMm/--------hMd.                                
+echo    MMy           MMy  +MM:         .dMh                                
+echo    MMy           MMy /NMo           -NMo`                              
+echo    MMy           MMy oso`            +ss.   
+
+ping localhost -n 2 >nul
+cls
+   
+echo  mms           mms        `ymd-         smmmmmmmmmmdyo-                        
+echo  MMy           MMy       `oMhNd`        yMM++++++++smMN/                       
+echo  MMy           MMy       +Nh.oMy`       yMM         `yMN-                      
+echo  MMy`          MMy      :NN. `hMs       yMM          +MN:                      
+echo  MMNdmmmmmmmmmmMMy     -mN/   .mM/      yMM       `./NMh.                      
+echo  MMdoooooooooooMMy    `mMy-..--+NN/     yMMmmmmmmmmNMms`                       
+echo  MMy           MMy   `yMMMMMMMMMMMN.    yMMooooooo+/:`                         
+echo  MMy           MMy   sMm:--------hMh.   yMM                                    
+echo  MMy           MMy  +MM:         .dMh   yMM                                    
+echo  MMy           MMy :mMo           -NMo` yMM                                    
+echo  ss+           ss+ oso`            +ss. +ss 
+
+ping localhost -n 2 >nul
+cls
+
+echo :mm:          :mm:        -dmy`         mmmmmmmmmmdhs/`   :mmmmmmmmmmdhs:`              
+echo /MM/          /MM/       `dNhMo`        MMd+++++++oyNMd-  /MMs+++++++odNNy`             
+echo /MM/          /MM/      `yMo.hN+        MMy         :mMd  /MM/        `/MMo             
+echo /MM/          /MM/      sMy` .NN:       MMy         `hMN  /MM/         `MMs             
+echo /MMmmmmmmmmmmmmMM/     /Mm.   /Nm-      MMy       `.sNMo  /MM/      ``:hMN-             
+echo /MMyooooooooooyMM/    /mN+....-yMd`     MMNmmmmmmmNNNm/`  /MMmmmmmmmmNMNh:              
+echo /MM/          /MM/   .NMMMMMMMMMMMy`    MMmooooooo+/-`    /MMyooooooo+:.                
+echo /MM/          /MM/  .dMy--------:mMs    MMy               /MM/                          
+echo /MM/          /MM/  hMd.         :MM+   MMy               /MM/                          
+echo /MM/          /MM/`oMN-           oMm/  MMy               /MM/                          
+echo -ss-          -ss-.ss/            .oso  ss+               -ss-  
+
+ping localhost -n 2 >nul
+cls
+
+echo   smm           smm         +mm+         :mmmmmmmmmmdhs-`   smmmmmmmmmmdy+-  `omm/`        `/mmo`  
+echo   yMM           yMM        :mmmN:        /MMy+++++++odNNy`  yMM++++++++smMN/` `oNNs`      `sNmo`   
+echo   yMM           yMM       .NN--NN.       /MM/        `/MMo  yMM         `yMN-   :mMh-    -hMm:     
+echo   yMM          `yMM      .dN+  +Nh.      /MM/         `MMs  yMM          +MN:    .yMN/  /NMy.      
+echo   yMMmmmmmmmmmmmNMM     `hMs`  `sMh`     /MM/      ``:hMN-  yMM       `./NMh.     `oNNssNNo`       
+echo   yMMooooooooooodMM    `oMN-....-NMs`    /MMmmmmmmmmNMNh-   yMMmmmmmmmmNMms`        :mMMm:         
+echo   yMM           yMM    +NMMMMMMMMMMN+    /MMyooooooo+:.`    yMMooooooo+/:`           /MM/          
+echo   yMM           yMM   :NM+--------+MN:   /MM/               yMM                      /MM/          
+echo   yMM           yMM  -mMs          sMm-  /MM/               yMM                      /MM/          
+echo   yMM           yMM `dMh.          .hMd` /MM/               yMM                      /MM/          
+echo   +ss           +ss :ss-            -ss/ -ss-               +ss                      -ss- 
+
+
+ping localhost -n 2 >nul
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo          hMMhsssssyymNM+:     :MMd     `MMMssssssyyhmNMs/  `ssyssssdMMmsssyssy.  yMM/           /MMh       sMMdsssssyyhdNdh+.        :oMNmMdo`     `-ymNy/`      ./hNmy-`                                   
+echo          hMM/     ``+yMms`   `:MMd   ` `MMM       ``/sMNh          oMMs          yMM+           /MMh   `   sMMo       .-+mNm/.      `oNNy/NNm-`      `sdMhs      sdMds.`                                    
+echo          hMM/       .sMmo     :MMd   ` `MMM   `     ./MNh          oMMs          yMM+           /MMh       sMM+          +yMy+     `/hMy/ sdM+:       `-mNN+-  -+NNm-`                                      
+echo          hMMo::::://hdN/-`    :MMd    `.MMM:::::/:/ohmMy+          oMMs          yMMs:::::::::::oMMh       sMMo          `/MNh    `.dmN-` -oNds`        -smNs::sNms-                                        
+echo          hMMMMMMMMMNMmh-` ``  :MMd     `MMMMMMMMMMMNdhs-`          oMMs          yMMMMMMMMMMMMMMMMMh       sMMo           :MNd    -/Mmh    /dNd/`    `   .:mddddm:.   `                                     
+echo          hMMo:::/::/hdmdo.`   :MMd     `MMM::::+yNNm/.`            oMMs          yMMs:::::::::::oMMh       sMMo          `/MMd    odMNNhhhhdNMNd-`         /hMMh/                                           
+echo          hMM/        -/NNd` ` :MMd     `MMM    ``shNds``           oMMs          yMM+           /MMh       sMMo          -+Mdy   -yMmmdddddddmNM/.          oMMo                                            
+echo          hMM/ `      `.NNN.   :MMd     `MMM      `/mNms.           oMMs          yMM+           /MMh       sMMo         `sdMy/  .ymN-``    ``-oMho          oMMo                                            
+echo          hMM/       ./yMmh.   :MMd     `MMM       ./dNNo-          oMMs          yMM/           /MMh       sMM+     `.-/sNNd:` .:Nmh          :dNh.`        sMMo                                            
+echo          hMMmddmmddmmmNds-` ` :MMd     `MMM      `` +yMNh-`        oMMs          yMM+           /MMh       sMMmddddddmmmmds.`  /yMs:          `:mmy. `      oMNo                                            
+echo         `shhhhhhhhhhyo/-.     -yhs     `hhh         `-yhy+.       `/hh+         `+yh:           -hho`      +yhhhhhhhyyso/.`    +yy: `           oyy-        /hh+`  
+
+
+ping localhost -n 2 >nul
+
+echo							.																		.
+echo							.																		.
+
+
+echo                                                       /+                                                                                             
+echo                                                      :dd+                  --                                                                        
+echo                                   `.                .ds-d-                +md-              :dy`                                                     
+echo                                  -hh`              `yh. oy`              od+:d:            -doyy`                                                    
+echo                                `omsd/        `     sd.  -d:    `y-      :m/  /h`          .hs``yo     `                                              
+echo                               `yh: oh`      ods   .d/   :d/   `smh.     yd   `d/         `yh`  -d-   sd.      `                                      
+echo                               od:  -d:     +mod/  `sdsshdo`   od/hs     hh   .d+    -o`  -d/   `d+  /ddo     .ds`                                    
+echo                               yh   `h+    :d+ +h`   .yd-`    /m+ :d:    -hhyhhs.   :dms` .do.`./d/ .hs:d-    +dyd-                                   
+echo                               sd`  `ho   -ds  .m-  .:hh-    `hy` `hy     `-d:`    .ds.ho  .+hdss/ `yh``ho   `ho`+h-                                  
+echo                               -hs/+yd:  `hh`  :d- :do++yo   `do   hd     :smy+.   yh` +d`   sy`   :d/ .do   -d-  oh.                                 
+echo                                `-sd/.   .my/+yd+  oh`  +y    +dyyhh/    -m...+y  -m:  :d- `+dds+` -do:yd-   :d-  -d/                                 
+echo                                /hyysyy`  -odo:.   oh   +y     .d+.`     /d   +s  .d+:/hy` -d.`.y`  +dso-    `ohsyhy.                                 
+echo                                oh`  +d`  :smyo-   sy   +s   -osdh/`     +d   +s   -sd+:`  /y   d`  oy        `.sm.`                                  
+echo                                sh`  +d  `do``od   sh   oo   so``.ys     :m   +s   `/d.    ss  `d`:shh:`     :ysyd/`                                  
+echo                                sh`  oh  .m:  +m`  oh`  s+   s+   oh     .m`  +s  /ssyy-   h+  `d.d:`.h:     s/``.m-                                  
+echo                                yh`  +d  `m:  /m`  +h`  s+   y/   +h     .m.  /s `h- `y+  `d/  `d-m.  s+     y:  `m-                                  
+echo                                yh`  /d` .m:  :m`  sy   y+  `h-   oy     -m`  :y .h`  y+  .d-  .d:m`  +s     y/  `m-                                  
+echo                               `hy   :m. `m/  :m-..ho  `do--:h`   +h:////+m.  :y:/h   yo--+d.  -h-m`  /s     h/  `d-                                  
+echo                            `.-:do   :msssd/  /msssm+  .dsoosy    oh++///+m-  :h/oy   yyooyd`  :dom.  :hsooo+d+   h/.`                                
+echo                     `-/+syhyhdddo   -d/.:d:  /m``.m:  .m-  -y    +y     `h/  /s :y   s-  +h   :h-m`  .h.....h+   yysyhyo/-.                          
+echo                  -+syso/:-+ydy+hy   -d: -d-  /m` +m:  .hy: -y    /h     -m+``/d+++   o/  os   /h-m`  `h.    yo   y+`./sddssyo:`                      
+echo                .oy+-``  :yy/.`+do```:m/ :d.  -m- :sssssss: .y    /h     `osssss+o/   o/  sy   /d+m`  `h-    ss   ss    -ho`.-oso-`                   
+echo               .hs.      sy`   -ssyyssy/ :d.  -d:   `````   -s    /h       ````  s/   o+  hd+/+sdyd/   hs:  -do-:/yh``.:sd+    `/yy/`                 
+echo               +d.       .oo/:.` `````` `yh-../do           +y` ``yh`            y+   oy- .:++/:-`sdssshh/  `+yyhhysyyss+-`      `:so`                
+echo               -y+.        .:+ossso+/::--oyyyyys+``         +yssssyo.     ```````shyyydd+.......--:+oooooosyyyyssso/-.`            `h/                
+echo                `/s+.          `.-::/+ooossssssssssssssssooooosyysssosssssssssssssyyyyyysssssssssssoo++//:--..```                  `yo                
+echo                  `+h-                    ``````````........------........`````````````````````                               ``.:+yy.                
+echo                   .ds                                                                                                     `-oyhys+-`                 
+echo                   :d+                                                                                            `.---.``.oh+ms`                     
+echo                   omy-.--:///:-.                                                                               -oysyysyyyho.:m:                      
+echo                   om/oosso+/++oss/.                                                           `.://:.`      `-sy+.`  ``.-.  oh`                      
+echo                   /d.          `-+yo:`     `.-:/+ooo+:.`               `..-.`              `-+yyo++oyyo+/:/oyy+.           .ho                       
+echo                   .d/             `/osso+osyyys+//::/oss+:.`        .:+yysssso:.``      `./yy+-`    `-:/+o+/-`             /d-                       
+echo                   `ho                `-::::-.`        `./ssso+/://oyyy+-`  `.:osssssssssyys:`                              sh                        
+echo                  `hy-`                                   `.:/++++/-.`         ``..-----.`                              `.-ds                        
+echo                    sdyys+:.`                                                                                   ``.-:/+osyyhd:                        
+echo                    oh..:+sysso/-.``                                                                  ```..-:/oosssso+/:--`sh`                        
+echo                    /d`    `.-:+ossss+/:-..```                                           ````..-:/++ooossso+/:-..``   ```` hs                         
+echo                    :d-`          `-:/+ossssssso+/:-..`````````````````......--::///++oosssssssoo+/:--.``          .:oooooodo                         
+echo                    .myyo:.`              ``..--:/+ossssssssssssssssssssssssoo++///::----..``        ```...`    `-sy+-.````hs                         
+echo                     do-/oys:``````                   ``````````````                            `.-/+osssssso/:+sy/`       oh`                        
+echo                     hs   `:ssossooo/.                                  `..------..-----....-:+ooo+/-.`    `.://-`         /d:                        
+echo                     yy      ``````-oy/.````..:/+++///-..``        ``.:+sso+//+++++++++++oo++/:.`                          .hs                        
+echo                     yy              ./oooooo+/:-----:/++ooo+/:://oooo/-`                                                `.:hd`                       
+echo                     hy`                 `                 `.-:::-.`                                                ``.:+syssd/                       
+echo                     ddo-.`                                                                                   ``.-/ossys/:. `hs                       
+echo                    `ds-+sso+:.``                                                                    ``..-/+ossyso/-.`       sh.                      
+echo                    /d/`  `-/+sssso+/:-..````````                                          ````.-//oossyso+/:-.`       `.:/+/+d:                      
+echo                   .dhhso/..``  `.--://+ooosssssoooo+++//::-...```````````````...----::/+oosssso+:--..`    ```     ``:oso/----d+                      
+echo                  .yh.`.:ossss/````````       ```...---:://+oossssssssssssssssssoo+++//::-.``          `-+ossoo+/:/syo:`     `ds                      
+echo                 `sd-        ./ooooooooo:`           `                               ```.```````   `./sso:.````-://-`         yh`                     
+echo              `-+shd+`                `.+y+-```.:/+oosso-       ``-:://:-.``      `.+sssooooosssoooss+-`                      oh`                     
+echo            `/yho:.:yy:`                 `:+ooo+/:-.```:h+``.-/+oo+/::::+oooo+//+oso/.         ``.`                           od.```                  
+echo            sh/`    `/hy/.`                             `+ooo+:.            `.--.`                                        `.-/hdssyso:`               
+echo           .d/        `/shs+-``                                                                                       `./oshs/----.-/yy.              
+echo           `ys.          `:oyhyo/-``                                                                            ``.:+shy+:.          `yy`             
+echo            .yh-`            `-+ohhyo/-```                                                              ````.-/oyhhs+:.               :d:             
+echo             `/hy+.`              `./+shhys+:.``                                              ````.-:/osyyhhyo+:.`                    `d+             
+echo               `:ohyo:.`                `.:+shhyso+//:--...``````````````````...--:////+oooossyyhhyso+/-.`                           `oh-             
+echo                  `-/oyhyso/-``               `.:://+osssyyhhhhhhhhhyhhhdhyyyssoo++///::::--...``                                  .+yo.              
+echo                        `-/+syhyyso+/:-.```                                                                                   `.:oyy/`                
+echo                               `.-:/+ooyyyyyyssoo+++//////::---...``````                                           ``..-:/+ossso+:`                   
+echo                                           ```..-:////++++ooossssyyyyyhhhhhyyyysssssssssoooooooooooooooooooooossyyyyysoo+/:.`                         
+                                                                                
+pause>nul
